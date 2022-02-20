d26fe767dff3b46ee57591a9525545f2bdaf7b80
Code With Stein <codewithstein@gmail.com>
2022-02-20 21:16:03 +0100

    part 3

--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+from product.models import Product
+
 def frontpage(request):
-    return render(request, 'core/frontpage.html')
+    products = Product.objects.all()[0:8]
+
+    return render(request, 'core/frontpage.html', {'products': products})
---
+++ product/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('name',)
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+    
+    def __str__(self):
+        return self.name
