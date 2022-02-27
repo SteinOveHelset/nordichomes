33d7fb289bccdd2bd057e37c17bc9a1901d8b454
Code With Stein <codewithstein@gmail.com>
2022-02-27 19:28:36 +0100

    part 5

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
+from django.db.models import Q
 from django.shortcuts import render
 
-from product.models import Product
+from product.models import Product, Category
 
 def frontpage(request):
     products = Product.objects.all()[0:8]
@@ -8,6 +9,23 @@ def frontpage(request):
     return render(request, 'core/frontpage.html', {'products': products})
 
 def shop(request):
+    categories = Category.objects.all()
     products = Product.objects.all()
 
-    return render(request, 'core/shop.html', {'products': products})
+    active_category = request.GET.get('category', '')
+
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    query = request.GET.get('query', '')
+
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    context = {
+        'categories': categories,
+        'products': products,
+        'active_category': active_category
+    }
+
+    return render(request, 'core/shop.html', context)
