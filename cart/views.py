fce31cff7c210f9d273aa3b5cbf72c0661a586eb
Code With Stein <codewithstein@gmail.com>
2022-03-18 06:39:03 +0100

    part 8

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .cart import Cart
+
+def add_to_cart(request, product_id):
+    cart = Cart(request)
+    cart.add(product_id)
+
+    return render(request, 'cart/menu_cart.html')
--- nordichomes/urls.py
+++ nordichomes/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
+from cart.views import add_to_cart
 from core.views import frontpage, shop
 from product.views import product
 
@@ -8,5 +9,6 @@ urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('admin/', admin.site.urls),
 ]
