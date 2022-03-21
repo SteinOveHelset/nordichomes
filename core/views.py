80dfd43fb41c02ab09948d2df050082c534c8974
Code With Stein <codewithstein@gmail.com>
2022-03-21 17:50:13 +0100

    part 10

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import login
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from product.models import Product, Category
+
+from .forms import SignUpForm
 
 def frontpage(request):
     products = Product.objects.all()[0:8]
@@ -9,9 +12,21 @@ def frontpage(request):
     return render(request, 'core/frontpage.html', {'products': products})
 
 def signup(request):
-    return render(request, 'core/signup.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
 
-def login(request):
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('/')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'core/signup.html', {'form': form})
+
+def login_old(request):
     return render(request, 'core/login.html')
 
 def shop(request):
--- nordichomes/urls.py
+++ nordichomes/urls.py
@@ -1,16 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path
 
 from cart.views import add_to_cart
-from core.views import frontpage, shop, signup, login
+from core.views import frontpage, shop, signup
 from product.views import product
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('signup/', signup, name='signup'),
-    path('login/', login, name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
