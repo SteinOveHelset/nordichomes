de9f5d01210ead1a19769bf99cd54f3c44ce1c8e
Code With Stein <codewithstein@gmail.com>
2022-04-03 12:43:07 +0200

    part 14

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import views
 from django.urls import path
 
-from core.views import frontpage, shop, signup
+from core.views import frontpage, shop, signup, myaccount, edit_myaccount
 from product.views import product
 
 urlpatterns = [
@@ -9,6 +9,8 @@ urlpatterns = [
     path('signup/', signup, name='signup'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('myaccount/', myaccount, name='myaccount'),
+    path('myaccount/edit/', edit_myaccount, name='edit_myaccount'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
+import re
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render, redirect
 
@@ -26,8 +28,22 @@ def signup(request):
 
     return render(request, 'core/signup.html', {'form': form})
 
-def login_old(request):
-    return render(request, 'core/login.html')
+@login_required
+def myaccount(request):
+    return render(request, 'core/myaccount.html')
+
+@login_required
+def edit_myaccount(request):
+    if request.method == 'POST':
+        user = request.user
+        user.first_name = request.POST.get('first_name')
+        user.last_name = request.POST.get('last_name')
+        user.email = request.POST.get('email')
+        user.username = request.POST.get('username')
+        user.save()
+
+        return redirect('myaccount')
+    return render(request, 'core/edit_myaccount.html')
 
 def shop(request):
     categories = Category.objects.all()
